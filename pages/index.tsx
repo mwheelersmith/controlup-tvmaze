eb56b32ae7b988b7f2a9e8c9a9be6a4f670134bf
Miles Wheeler-Smith <miles@mileswheelersmith.com>
2025-04-08 09:30:16 +0100

    Remove Geist font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
 import Search from "@/components/search";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
+    <div className="font-sans antialiased">
       <main className="flex flex-col w-sm mx-auto mt-32">
         <Search />
       </main>
